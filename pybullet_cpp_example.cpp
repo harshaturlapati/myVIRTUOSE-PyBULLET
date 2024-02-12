8edd41d1b70e2ca7e77d67e44bd025b85032de9e
Sri Harsha Turlapati <32917475+harshaturlapati@users.noreply.github.com>
2024-02-12 20:12:45 +0800

    get f_prime and t_prime.

--- pybullet_cpp_example.cpp
+++ pybullet_cpp_example.cpp
@@ -7,7 +7,6 @@
 #include <Windows.h>
 #include <iostream>
 
-
 class myBULLET {
 public:
     float time_step;
@@ -19,11 +18,23 @@ public:
     b3ContactInformation contactInfo;
     int world, actor;
     float force[3], torque[3];
+    float f_prime[3], t_prime[3];
     float v1[3], v2[3];
     float cross_v1v2[3];
     float positiononA[3], positiononB[3];
     int N_con;
     float R_actor[3][3];
+    float RT_actor[3][3];
+    float minus_RT_actor[3][3];
+    btVector3 pos_actor;
+    btQuaternion quat_actor;
+    float norm_quat;
+    float Ax[3];
+    float AB[3][3];
+    float p_cross[3][3];
+
+    //float quat_actor[4];
+    //float pos_actor[3];
 
     void init() {
         b3PhysicsClientHandle client = b3ConnectSharedMemory(SHARED_MEMORY_KEY);
@@ -65,11 +76,98 @@ public:
         cross_v1v2[0] = v1[1] * v2[2] - v2[1] * v1[2];
         cross_v1v2[1] = v2[0] * v1[2] - v1[0] * v2[2];
         cross_v1v2[2] = v1[0] * v2[1] - v2[0] * v1[1];
-        std::cout << cross_v1v2[0] << cross_v1v2[1] << cross_v1v2[2] << std::endl;
+        std::cout << "Cross product result is : " << cross_v1v2[0] << "," << cross_v1v2[1] << "," << cross_v1v2[2] << std::endl;
     }
 
-    void getROT() {
-        
+    void multiplyAx(float A[3][3], float x[3]) {
+        Ax[0] = A[0][0] * x[0] + A[0][1] * x[1] + A[0][2] * x[2];
+        Ax[1] = A[1][0] * x[0] + A[1][1] * x[1] + A[1][2] * x[2];
+        Ax[2] = A[2][0] * x[0] + A[2][1] * x[1] + A[2][2] * x[2];
+    }
+
+    void multiplyAB(float A[3][3], float B[3][3]) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                AB[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                AB[i][j] = AB[i][j] + A[i][j] * B[j][i];
+            }
+        }
+    }
+
+    void getSE3() {
+        api.getBasePositionAndOrientation(actor, pos_actor, quat_actor);
+        std::cout << "position - x = " << pos_actor[0] << ", y = " << pos_actor[1] << ", z = " << pos_actor[2] << std::endl;
+        float x, y, z, w, a11, a12, a13, a21, a22, a23, a31, a32, a33;
+        std::cout << "quaternion - x = " << quat_actor.getX() << ", y = " << quat_actor.getY() << ", z = " << quat_actor.getZ() << ", w = " << quat_actor.getW() << std::endl;
+        x = quat_actor.getX();
+        y = quat_actor.getY();
+        z = quat_actor.getZ();
+        w = quat_actor.getW();
+
+
+        norm_quat = x * x + y * y + z * z + w * w;
+        if (norm_quat == 0)
+        {
+            R_actor[0][0] = 1;
+            R_actor[0][1] = 0;
+            R_actor[0][2] = 0;
+            R_actor[1][0] = 0;
+            R_actor[1][1] = 1;
+            R_actor[1][2] = 0;
+            R_actor[2][0] = 0;
+            R_actor[2][1] = 0;
+            R_actor[2][2] = 1;
+        }
+        else
+        {
+            a11 = (w * w + x * x + y * y - z * z) / norm_quat;
+            a12 = 2 * (x * y - w * z) / norm_quat;
+            a13 = 2 * (x * z + w * y) / norm_quat;
+            a21 = 2 * (x * y + w * z) / norm_quat;
+            a22 = (w * w - x * x + y * y - z * z) / norm_quat;
+            a23 = 2 * (y * z - w * x) / norm_quat;
+            a31 = 2 * (x * z - w * y) / norm_quat;
+            a32 = 2 * (y * z + w * x) / norm_quat;
+            a33 = (w * w - x * x - y * y + z * z) / norm_quat;
+            R_actor[0][0] = a11;
+            R_actor[0][1] = a12;
+            R_actor[0][2] = a13;
+            R_actor[1][0] = a21;
+            R_actor[1][1] = a22;
+            R_actor[1][2] = a23;
+            R_actor[2][0] = a31;
+            R_actor[2][1] = a32;
+            R_actor[2][2] = a33;
+        }
+
+        RT_actor[0][0] = R_actor[0][0];
+        RT_actor[0][1] = R_actor[1][0];
+        RT_actor[0][2] = R_actor[2][0];
+        RT_actor[1][0] = R_actor[0][1];
+        RT_actor[1][1] = R_actor[1][1];
+        RT_actor[1][2] = R_actor[2][1];
+        RT_actor[2][0] = R_actor[0][2];
+        RT_actor[2][1] = R_actor[1][2];
+        RT_actor[2][2] = R_actor[2][2];
+
+        minus_RT_actor[0][0] = -RT_actor[0][0];
+        minus_RT_actor[0][1] = -RT_actor[1][0];
+        minus_RT_actor[0][2] = -RT_actor[2][0];
+        minus_RT_actor[1][0] = -RT_actor[0][1];
+        minus_RT_actor[1][1] = -RT_actor[1][1];
+        minus_RT_actor[1][2] = -RT_actor[2][1];
+        minus_RT_actor[2][0] = -RT_actor[0][2];
+        minus_RT_actor[2][1] = -RT_actor[1][2];
+        minus_RT_actor[2][2] = -RT_actor[2][2];
+
+        std::cout << "Rotation matrix is" << std::endl;
+        std::cout << R_actor[0][0] << "," << R_actor[0][1] << "," << R_actor[0][2] << std::endl;
+        std::cout << R_actor[1][0] << "," << R_actor[1][1] << "," << R_actor[1][2] << std::endl;
+        std::cout << R_actor[2][0] << "," << R_actor[2][1] << "," << R_actor[2][2] << std::endl;
     }
 
     void evalCON() {
@@ -77,18 +175,29 @@ public:
         N_con = contactInfo.m_numContactPoints;
         std::cout << "number of contacts are : " << N_con << std::endl;
 
+        force[0] = 0;
+        force[1] = 0;
+        force[2] = 0;
+
+
+        torque[0] = 0;
+        torque[1] = 0;
+        torque[2] = 0;
+
+        f_prime[0] = 0;
+        f_prime[1] = 0;
+        f_prime[2] = 0;
+
+
+        t_prime[0] = 0;
+        t_prime[1] = 0;
+        t_prime[2] = 0;
+
         if (N_con > 0)
         {
             //std::cout << contactInfo.m_contactPointData[j].m_bodyUniqueIdA << std::endl;
             //std::cout << contactInfo.m_contactPointData[j].m_contactDistance << std::endl;
-            force[0] = 0;
-            force[1] = 0;
-            force[2] = 0;
-
-
-            torque[0] = 0;
-            torque[1] = 0;
-            torque[2] = 0;
+          
 
             for (int k = 0; k < N_con; k++)
             {
@@ -168,9 +277,43 @@ public:
                 std::cout << "contact location on B in WS is : x =" << positiononB[0] << ": y = " << positiononB[1] << ": z = " << positiononB[2] << std::endl;
                 std::cout << "contact number " << k << " is : " << f << std::endl;*/
             }
-            std::cout << "Total force is : x = " << force[0] << ", y = " << force[1] << ", z = " << force[2] << std::endl;
-            std::cout << "Total torque is : x = " << torque[0] << ", y = " << torque[1] << ", z = " << torque[2] << std::endl;
+            
         }
+
+        getSE3();
+
+        p_cross[0][0] = 0;
+        p_cross[0][1] = -pos_actor[2];
+        p_cross[0][2] = pos_actor[1];
+        p_cross[1][0] = pos_actor[2];
+        p_cross[1][1] = 0;
+        p_cross[1][2] = -pos_actor[0];
+        p_cross[2][0] = -pos_actor[1];
+        p_cross[2][1] = pos_actor[0];
+        p_cross[2][2] = 0;
+
+        multiplyAx(RT_actor,force);
+        f_prime[0] = Ax[0];
+        f_prime[1] = Ax[1];
+        f_prime[2] = Ax[2];
+
+        multiplyAx(RT_actor, torque);
+        t_prime[0] = 0 + Ax[0];
+        t_prime[1] = 0 + Ax[1];
+        t_prime[2] = 0 + Ax[2];
+
+        multiplyAB(minus_RT_actor, p_cross);
+        multiplyAx(AB, force);
+        t_prime[0] = t_prime[0] + Ax[0];
+        t_prime[1] = t_prime[1] + Ax[1];
+        t_prime[2] = t_prime[2] + Ax[2];
+
+        std::cout << "Total force is : x = " << force[0] << ", y = " << force[1] << ", z = " << force[2] << std::endl;
+        std::cout << "Total torque is : x = " << torque[0] << ", y = " << torque[1] << ", z = " << torque[2] << std::endl;
+
+        std::cout << "Total f_prime is : x = " << f_prime[0] << ", y = " << f_prime[1] << ", z = " << f_prime[2] << std::endl;
+        std::cout << "Total t_prime is : x = " << t_prime[0] << ", y = " << t_prime[1] << ", z = " << t_prime[2] << std::endl;
+
 
     }
 
@@ -184,10 +327,23 @@ public:
 int main()
 {
     myBULLET SIM(0.01f);
+    btVector3 pos;
+    btQuaternion quat;
     
+
     printf("Press Q to exit loop\n");
     while (!(GetKeyState('Q') & 0x8000)){
         SIM.evalCON();
+
+
+
+
+        //SIM.getSE3();
+        /*SIM.api.getBasePositionAndOrientation(SIM.actor, pos, quat);
+        std::cout << "position - x = " << pos[0] << ", y = " << pos[1] << ", z = " << pos[2] << std::endl;
+        std::cout << "quaternion - x = " << quat.getX() << ", y = " << quat.getY() << ", z = " << quat.getZ() << ", w = " << quat.getW() << std::endl;*/
+
+        //SIM.quat_actor;
         //SIM.api.resetBasePositionAndOrientation(SIM.actor, btVector3(btScalar(0), btScalar(0), btScalar(0.1)), btQuaternion(btScalar(0), btScalar(0), btScalar(0), btScalar(1)));
 
         //SIM.api.applyExternalForce(SIM.actor, -1, btVector3(btScalar(0), btScalar(0), btScalar(-0.1)), btVector3(btScalar(0), btScalar(0), btScalar(0)), 0);
