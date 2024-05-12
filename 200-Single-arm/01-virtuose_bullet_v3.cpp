6e1019d5466529985b65b38b5a6bf208ec2c0d05
Sri Harsha Turlapati <sriharsha.turlapati@staff.main.ntu.edu.sg>
2024-05-12 22:06:09 +0800

    reorganizing single arm and dual arm folders.

---
+++ 200-Single-arm/01-virtuose_bullet_v3.cpp
@@ -0,0 +1,64 @@
+#include <myINCLUDES.h>                 // baked in
+#include "VirtuoseAPI.h"                // comes from Haption
+#include <myVIRTUOSE_v3.h>              // baked in
+#include <myVIRTUOSE_UDP.h>             // baked in
+#include <myVIRTUOSE_LOGGING.h>         // baked in
+#include <myBULLET_v3.h>
+
+int main()
+{
+    // UDP class object
+    myVIRTUOSE_UDP myUDP(27017, 27018, "127.0.0.1", "127.0.0.1");
+    float input_pos[7] = { 0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,1.0f };
+    myUDP.setup_UDP();
+
+    // Virtuose object attributes
+    const char* myPORT = "127.0.0.1#53210";
+    float myFORCEFACTOR = 1.0f, mySPEEDFACTOR = 1.0f, delta_t = 0.01f;
+
+    // Virtuose object definition
+    float k = 10;
+    ARM RightARM("127.0.0.1#53210", myFORCEFACTOR, mySPEEDFACTOR, delta_t, k);
+    RightARM.name = "RightARM";
+    RightARM.quick_start(); // always needs to be done
+    RightARM.debug_getPOS(); // always needs to be done
+
+
+    // Impedance control parameters
+
+    float b_r = 0.5;
+    float b = 4;
+    myBULLET SIM(delta_t, b, b_r);
+
+    // to be sent to Haption at the end - may consider making this part of the CMD structure?
+
+    printf("Press Q to exit loop\n");
+    while (!(GetKeyState('Q') & 0x8000)) {
+
+        // Query Haption state and output through UDP
+        myUDP.UDP_send_recv_v3(RightARM.getPOS()); // getPOS() computes frame H
+
+        // Compute object frame O
+        SIM.getSIM_state();
+
+        // Computer f_i_plus
+        RightARM.compute_f_cmd(SIM.O);
+
+        // Apply forces in simulation
+        SIM.apply_control_forces(RightARM.cmd.f_i_plus, RightARM.cmd.e_i);
+
+        // Apply forces on handle
+        RightARM.render_W_cmd();
+
+        // Simulation step
+        SIM.api.stepSimulation();
+        Sleep(delta_t);
+    }
+
+    // Haption clean up
+
+    myUDP.cleanup();
+    RightARM.quick_stop();
+
+    return 0;
+}
---
+++ 300-Dual-arm/01-dual_arm_virtuose_v4.cpp
@@ -0,0 +1,95 @@
+#include <myINCLUDES.h>                 // baked in
+#include "VirtuoseAPI.h"                // comes from Haption
+#include <myVIRTUOSE_v3.h>              // baked in
+#include <myVIRTUOSE_UDP.h>             // baked in
+#include <myVIRTUOSE_LOGGING.h>         // baked in
+#include <myBULLET_v4.h>
+
+
+int main()
+{
+
+    float myDT = 0.01f;
+
+    // Right Arm desired configuration
+    // 192.168.101.53
+    // Port to API : 53211
+
+    // Left Arm desired configuration
+    // 192.168.100.53
+    // Port to API : 53210
+
+    // // // LEFT ARM
+    // UDP class object
+    myVIRTUOSE_UDP myUDP_L(27015, 27016, "127.0.0.1", "127.0.0.1");
+    myUDP_L.setup_UDP();
+
+    // Virtuose object attributes
+    const char* myPORT_L = "127.0.0.1#53210";
+    float myFORCEFACTOR_L = 1.0f, mySPEEDFACTOR_L = 1.0f;
+
+    // Virtuose object definition
+    float k_L = 10;
+    ARM ARM_L("127.0.0.1#53210", myFORCEFACTOR_L, mySPEEDFACTOR_L, myDT, k_L);
+    ARM_L.name = "ARM_L";
+    ARM_L.quick_start(); // always needs to be done
+    ARM_L.debug_getPOS(); // always needs to be done
+
+
+    // // // RIGHT ARM
+    // UDP class object
+    myVIRTUOSE_UDP myUDP_R(27017, 27018, "127.0.0.1", "127.0.0.1");
+    myUDP_R.setup_UDP();
+
+    // Virtuose object attributes
+    const char* myPORT_R = "127.0.0.1#53210"; // change to 53211 tomorrow
+    float myFORCEFACTOR_R = 1.0f, mySPEEDFACTOR_R = 1.0f;
+
+    // Virtuose object definition
+    float k_R = 10;
+    ARM ARM_R("127.0.0.1#53210", myFORCEFACTOR_R, mySPEEDFACTOR_R, myDT, k_R);
+    ARM_R.name = "ARM_R";
+    ARM_R.quick_start(); // always needs to be done
+    ARM_R.debug_getPOS(); // always needs to be done
+
+    // Impedance control parameters
+
+    float b_r = 0.5;
+    float b = 4;
+    myBULLET SIM(myDT, b, b_r);
+
+    // to be sent to Haption at the end - may consider making this part of the CMD structure?
+
+    printf("Press Q to exit loop\n");
+    while (!(GetKeyState('Q') & 0x8000)) {
+
+        // Query Haption state and output through UDP
+        myUDP_R.UDP_send_recv_v3(ARM_R.getPOS()); // getPOS() computes frame H
+        myUDP_L.UDP_send_recv_v3(ARM_L.getPOS()); // getPOS() computes frame H
+
+        // Compute object frame O
+        SIM.getSIM_state();
+
+        // Computer f_i_plus
+        ARM_L.compute_f_cmd(SIM.O[0]); // need to make this a loop
+        ARM_R.compute_f_cmd(SIM.O[1]); // need to make this a loop
+
+        // Apply forces in simulation
+        SIM.apply_control_forces_DUAL(ARM_L.cmd, ARM_R.cmd);
+
+        // Apply forces on handle
+        ARM_L.render_W_cmd();
+        ARM_R.render_W_cmd();
+
+        // Simulation step
+        SIM.api.stepSimulation();
+        Sleep(myDT);
+    }
+
+    // Haption clean up
+
+    myUDP_R.cleanup();
+    ARM_R.quick_stop();
+
+    return 0;
+}
